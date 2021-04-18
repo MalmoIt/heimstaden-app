6c49e9f2b5adec25aa9d3e09ce5795496eec09b9
Anders R. Malmo <anders@malmoit.no>
2021-04-18 09:08:42 +0200

    Initial macOS support XF

---
+++ src/HeimstadenApp/HeimstadenApp.Mac/AppDelegate.cs
@@ -0,0 +1,47 @@
+﻿using AppKit;
+using Foundation;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace HeimstadenApp.Mac
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow window;
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            window.Title = "Hello XF Mac";
+            window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override NSWindow MainWindow => window;
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Insert code here to initialize your application
+            Forms.Init();
+            LoadApplication(new App(new macOSInitializer()));
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+
+    public class macOSInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register any platform specific implementations
+        }
+    }
+}
